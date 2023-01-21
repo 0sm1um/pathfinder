329c51c891224e9e593f90f408ad7fb5c1f30299
John Hiles <hilesj@vcu.edu>
2023-01-21 11:05:23 -0500

    Implemented path generator along grid.

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import numpy as np
 
 
 def generate_dataset():
-    grid = specify_map(num_tiles = 3)
+    matrix = specify_map(num_tiles = 6)
     
-    start = grid.node(0, 0)
-    end = grid.node(5, 5)
-    finder = AStarFinder()
-    path, runs = finder.find_path(start, end, grid)
     
-    print('operations:', runs, 'path length:', len(path))
-    print(grid.grid_str(path=path, start=start, end=end))
+    paths = generate_paths(matrix)
+    
+    'print paths'
+    
+    
     
     print('hello world')
 
@@ -29,9 +28,8 @@ def specify_map(num_tiles):
 
     Returns
     -------
-    grid : pathfinding.grid
+    matrix : numpy.array
         The "map" which is used for pathfinding.
-
     '''
     tile = np.array([
                      [1,0,0,0,0,0],
@@ -41,20 +39,52 @@ def specify_map(num_tiles):
                      [1,0,0,0,0,0],
                      [1,1,1,1,1,1]
                      ])
-    
+
     # Matrix Operations to define "large" space.
     
     matrix = np.concatenate([tile for e in range(num_tiles)])
     matrix = np.concatenate([matrix for e in range(num_tiles)],axis=1)
-    
+
     # These final rows are added to make the map symmetrical.
     
     matrix = np.concatenate([np.ones([1,matrix.shape[0]]),matrix])
-    matrix = np.concatenate([matrix,np.ones([matrix.shape[0],1])],axis=1)   
+    matrix = np.concatenate([matrix,np.ones([matrix.shape[0],1])],axis=1)
+    return matrix
+def generate_paths(matrix):
     grid = Grid(matrix=matrix)
-    return grid
-def generate_trajectory(start, end):
-    pass
+    finder = AStarFinder()
+    paths = []
+    boundary_index = matrix.shape[0]-1
+    
+    for i in range(boundary_index):
+        grid.cleanup()
+        start = grid.node(0,i) # Left Wall
+        for j in range(boundary_index):
+            grid.cleanup()
+            end = grid.node(boundary_index,j) # Right Wall
+            path, runs = finder.find_path(start,end,grid)
+            # Map from coordinates to directional sequence
+            # Legality Check goes here
+            paths.append(path)
+        
+    for i in range(boundary_index):
+        grid.cleanup()
+        start = grid.node(i,0) # Bottom Wall
+        for j in range(boundary_index):
+            grid.cleanup()
+            end = grid.node(j,boundary_index) # Top Wall
+            path, runs = finder.find_path(start,end,grid)
+            # Map from coordinates to directional sequence
+            # Legality Check goes here
+            paths.append(path)
+    
+    ''' This is a block of code to print trajectories.
+    for i in range(len(paths)):
+        print(grid.grid_str(path=paths[i], start=start, end=end))
+    print(len(paths))
+    '''
+    return paths
+
 
 def check_legality():
     pass
